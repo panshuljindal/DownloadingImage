a6be3c05a569ed8b00ee8bcaa41b196eb1cfcf51
panshuljindal <60618820+panshuljindal@users.noreply.github.com>
2020-11-21 15:46:34 +0530

    First Commit

---
+++ app/src/main/java/com/panshuljindal/downloadingimage/MainActivity.java
@@ -0,0 +1,59 @@
+package com.panshuljindal.downloadingimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    public void downloadImage(View view){
+        Log.i("Info","Button Pressed");
+        ImageDownloader task = new ImageDownloader();
+        Bitmap bitmap=null;
+        try {
+            bitmap = task.execute("https://image.shutterstock.com/image-illustration/seamless-blue-background-reliefrough-surface-260nw-1039499668.jpg").get();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        imageView.setImageBitmap(bitmap);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+
+
+    }
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+                InputStream in = urlConnection.getInputStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(in);
+                return bitmap;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+}
